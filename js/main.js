8c214bd163cb7b13343c873ae88f7b3447bb9d18
selvinraja4426 <selvinraja442006@gmail.com>
2026-05-12 15:58:17 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function func_btn1(){
+    console.log("func_btn1 is coming")
+    document.getElementById('btn_time1').innerHTML='2.30AM';
+
+
+function func_btn2()
+    console.log("func_btn2 is coming")
+    document.getElementById(func_btn2).innerHTML='11;00PM';
+
+
+function func_btn3()
+    console.log("func_btn3 is coming")
+    document.getElementById(func_btn3).innerHTML='12:45PM';
+
+
+function func_btn4()
+    console.log("func_btn4 is coming")
+    document.getElementById(func_btn4).innerHTML='3:00AM'
+}
+
